389e2cfcdb7d8292f3451b61b9ea6001f6de302c
Sohomx <sohom377@gmail.com>
2021-10-27 16:09:05 +0530

    done with header items + responsive☑

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,26 @@
 import React from "react";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
 
 function Header() {
   return (
-    <header className="">
-      <h1>This is the header</h1>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
       <img
         src="https://www.techadvisor.co.uk/cmsdata/features/3779153/hulu_logo_copy_thumb1200_16-9.png"
         alt="hulu-logo"

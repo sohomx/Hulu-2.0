022cda8d2b75d321d9d9cfa90b7b2f97bd1b0df4
Sohomx <sohom377@gmail.com>
2021-10-27 21:07:07 +0530

    make the api work next

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
       <Head>
@@ -14,8 +16,26 @@ export default function Home() {
       <Header />
       {/* Navbar */}
       <Nav />
-
       {/* Results */}
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const type = context.query.type;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3/${requests[type]?.url}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
